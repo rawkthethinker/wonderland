01e83d970ebbfc8f69d7111c5683ae99259c259f
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-04-15 12:58:33 +0200

    first commit

---
+++ precache-manifest.ebeeda797ead0922a7d49178f85609c1.js
@@ -0,0 +1,52 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90033233a3852e928dbef9d3fc254cdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de54a8116fa3e7c30f47",
+    "url": "/static/css/main.46bf36dc.chunk.css"
+  },
+  {
+    "revision": "3f50e0ab7e9d69b218f2",
+    "url": "/static/js/2.89fffa95.chunk.js"
+  },
+  {
+    "revision": "de54a8116fa3e7c30f47",
+    "url": "/static/js/main.c203aa55.chunk.js"
+  },
+  {
+    "revision": "a4ef66a5e76d70a4224b",
+    "url": "/static/js/runtime~main.212cec6f.js"
+  },
+  {
+    "url": "/static/media/054ffd1ba6a6c109b3ccee195074f928.054ffd1b.ttf"
+  },
+  {
+    "url": "/static/media/0a7742378df01f3c4fc2df2d53159ea9.0a774237.ttf"
+  },
+  {
+    "url": "/static/media/0aee630bf5cb8742755867ae9240daae.0aee630b.ttf"
+  },
+  {
+    "url": "/static/media/5e81217f7e141ed72533cde11d1252fa.5e81217f.ttf"
+  },
+  {
+    "url": "/static/media/61d5c4b568c2c68965ad8a35ebb04576.61d5c4b5.ttf"
+  },
+  {
+    "url": "/static/media/710cedaf67c4c630429aa1b827f56fea.710cedaf.ttf"
+  },
+  {
+    "url": "/static/media/75731629e00a1986098e9849fadadc82.75731629.jpg"
+  },
+  {
+    "url": "/static/media/7da201004f3c567bae2df158acb0b639.7da20100.ttf"
+  },
+  {
+    "url": "/static/media/a7ab6767910d27444c43e22abc777ba4.a7ab6767.jpg"
+  },
+  {
+    "url": "/static/media/ef8e5c06089f53886f3f4bb2940b027d.ef8e5c06.ttf"
+  }
+]);
